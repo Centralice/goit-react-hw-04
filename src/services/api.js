5f4f053879598225cd0d94af4a6d7f8376ce239e
Centralice <108938330+Centralice@users.noreply.github.com>
2024-08-04 15:16:28 +0200

    params

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,14 @@
 import axios from "axios";
 
-export const fetchData = async (query) => {
+export const fetchData = async (query, resultNumber = 1) => {
   const response = await axios.get(
-    `https://api.unsplash.com/search/photos?client_id=SGR-b_RMZ6ScDLeVlF4_miV43r2BL_2fZC8eKwByhiA&query=${query}`
+    "https://api.unsplash.com/search/photos?client_id=SGR-b_RMZ6ScDLeVlF4_miV43r2BL_2fZC8eKwByhiA",
+    {
+      params: {
+        query,
+        per_page: resultNumber,
+      },
+    }
   );
   return response.data;
 };
